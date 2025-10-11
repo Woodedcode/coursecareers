b78a829498e72ddc2420196760742afe158180af
Ryan <Woodedcode@gmail.com>
2025-10-10 22:33:36 -0500

    Add files via upload

---
+++ coursecareers/js/Arithmetic-Operators/AR.js
@@ -0,0 +1,30 @@
+/*
++
+-
+*
+/
+** - exponent
+% - remainder
+++ - increment
+-- - decrement
+*/
+let x = 10
+let y = 20
+
+
+console.log(z)
+
+/*
+B - brackets
+E - exponents
+D - division
+M - multiplication
+A - addition
+S - subtraction
+*/
+
+
+
+
+
+
