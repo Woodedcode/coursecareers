1d5ba23a9ed48bbe8636916d0ef1b8f568317f08
Ryan <Woodedcode@gmail.com>
2025-10-10 22:32:43 -0500

    Add files via upload

---
+++ coursecareers/js/Data Types/data-types.js
@@ -0,0 +1,36 @@
+/*
+// STRING
+"this is a string"
+'this is also a string'
+'5' , "55"
+
+// NUMBER
+1
+-9
+0.34
+
+// BOOLEAN
+true = 1
+false = 0
+
+// UNDEFINED
+undefined
+
+// NULL
+null
+
+// OBJECT
+{
+    x: 1
+}
+
+// NaN
+NaN
+*/
+
+console.log(typeof "hello")
+console.log(typeof 1)
+console.log(typeof true)
+console.log(typeof undefined)
+console.log(typeof {})
+console.log(typeof NaN)
