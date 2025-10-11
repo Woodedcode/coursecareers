b23cf8bb5a7b1a680a3f992deb8bb1ed7a9aa2be
Ryan <Woodedcode@gmail.com>
2025-10-10 22:33:17 -0500

    Add files via upload

---
+++ coursecareers/js/Variables/variables.js
@@ -0,0 +1,37 @@
+var x = 10;
+console.log(x)
+// descriptive name
+// var myNumber = 69
+// console.log(myNumber)
+// myNumber = 2
+// console.log(myNumber)
+
+// myNumber = myNumber + 2
+// console.log(myNumber)
+
+// printing multi variables
+var name1 = "Ryan"
+var name2 = "Bob"
+
+console.log(name1,name2)
+console.log(name1,name2, 1)
+
+name2 = name1
+console.log(name1,name2)
+name2 = 'Jill'
+console.log(name1,name2)
+
+// constant variable
+const aNumber = 22;
+
+
+// variable that can be changed
+
+let someNumber = 22
+someNumber = 1
+
+// use let and const. var comes down to scope
+
+let myNumber;
+myNumber = 3
+console.log(myNumber)
