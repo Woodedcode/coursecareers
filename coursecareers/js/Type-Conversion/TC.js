9a7dcab8cae912ab95cd756133c3f0a1c86a63e1
Ryan <Woodedcode@gmail.com>
2025-10-10 22:34:13 -0500

    Add files via upload

---
+++ coursecareers/js/Type-Conversion/TC.js
@@ -0,0 +1,21 @@
+// const x = true
+// const y = 10
+// const result = x + y
+// console.log(result)
+
+// parseInt
+
+
+// const name = "ryan"
+// const age = 36
+// const sentence = "Hi my name is " + name + ' and I am ' + age.toString() + ' years old'
+
+console.log(2 + (3).toString())
+
+const year = '1989'
+const age = 36
+
+console.log(+year + 46)
+
+
+
