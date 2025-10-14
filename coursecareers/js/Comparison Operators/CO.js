6de2bf11cba3bc2184579eaacaa4e6aed29018f7
Ryan <Woodedcode@gmail.com>
2025-10-13 21:39:54 -0500

    Add files via upload

---
+++ coursecareers/js/Comparison Operators/CO.js
@@ -0,0 +1,25 @@
+/*
+== - equal
+=== - strict equal to
+!= - not equal to
+!== - strict not equal to
+> - greater than
+< - less than
+<= - less than or equal to
+>= - greater than or equal to
+*/
+
+// const value = 5 == 5; // true
+// const value1 = 5 == 6; // false
+// console.log(value)
+// console.log(value1)
+
+// const notValue = 5 != "6";
+// console.log(notValue)
+
+
+// const str = 'hello'
+// console.log(str.charCodeAt(0))
+
+const value = 10 === 10
+console.log(value)
