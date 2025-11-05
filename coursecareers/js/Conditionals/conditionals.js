ae1967477b7a94394a3f4dedad90c7c2e0996b8a
Ryan <Woodedcode@gmail.com>
2025-11-04 22:38:00 -0600

    Add files via upload

---
+++ coursecareers/js/Conditionals/conditionals.js
@@ -0,0 +1,41 @@
+// if, else, else if
+
+const age = 13
+
+if (age >= 16) {
+    console.log("You can drive")
+} else if (age === 15) {
+    console.log('only one more year')
+} else if (age === 14) {
+    console.log('only two more years')
+} else {
+    console.log("you can NOT drive")
+}
+
+/*
+if (age > 80) {
+    console.log("you are too old to drive")
+}
+*/
+
+console.log("Thanks for playing")
+
+if (1) { 
+    console.log("true");
+}
+
+
+const x = undefined
+if (x) {
+    console.log("true")
+}
+
+
+
+
+
+
+
+
+
+
