8017f98c4a246e9a2f518e77185f666a6e50ccfe
Ryan <Woodedcode@gmail.com>
2025-11-04 22:12:11 -0600

    Add files via upload

---
+++ coursecareers/js/Logical Operators/LO.js
@@ -0,0 +1,41 @@
+/*
+&& - and
+|| - or
+! - negate current boolean
+*/
+
+//const and = true && false
+//console.log(and)
+//const and = x && y && z
+//console.log(and)
+
+// Truth Table
+/*
+x y z 
+0 0 0
+0 1 1
+1 0 0
+1 1 1
+*/
+
+// const and = false || false
+// console.log(and)
+//const and = true || true - true
+//const and = true || false - true
+//const and = false || false - false
+
+// const and = !true
+// console.log(and)
+
+// const and = !(5 < 4)
+// console.log(and) // true
+
+const and = (6 + 2) < 9) && ((-9 + (27 % 3)) == 2) || true
+console.log(and)
+
+
+
+
+
+
+
