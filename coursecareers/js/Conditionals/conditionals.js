9a3ddc128d647af3b292c7b8e68092cadc11ffba
Ryan <Woodedcode@gmail.com>
2025-11-05 21:41:48 -0600

    Refactor conditionals and add age prompt
    Comment out existing age checks and add prompt for user input.

--- coursecareers/js/Conditionals/conditionals.js
+++ coursecareers/js/Conditionals/conditionals.js
@@ -1,16 +1,16 @@
 // if, else, else if
 
-const age = 13
+// const age = 13
 
-if (age >= 16) {
-    console.log("You can drive")
-} else if (age === 15) {
-    console.log('only one more year')
-} else if (age === 14) {
-    console.log('only two more years')
-} else {
-    console.log("you can NOT drive")
-}
+// if (age >= 16) {
+//     console.log("You can drive")
+// } else if (age === 15) {
+//     console.log('only one more year')
+// } else if (age === 14) {
+//     console.log('only two more years')
+// } else {
+//     console.log("you can NOT drive")
+// }
 
 /*
 if (age > 80) {
@@ -18,19 +18,28 @@ if (age > 80) {
 }
 */
 
-console.log("Thanks for playing")
+// console.log("Thanks for playing")
 
-if (1) { 
-    console.log("true");
-}
-
-
-const x = undefined
-if (x) {
-    console.log("true")
-}
+// if (1) { 
+//     console.log("true");
+// }
 
 
+// const x = undefined
+// if (!x) {
+//     console.log("true");
+// }
+
+const age = prompt("How old are you?")
+console.log(typeof age)
+
+// if (age >= 12 && age < 20) {
+//     alert("You can ride the rollercoaster")
+// } else if (age >= 40){
+//     alert("You can only ride with someone between the ages of 12 and 20")
+// } else {
+//     alert("You can NOT ride the rollercoaster")
+// }
 
 
 
