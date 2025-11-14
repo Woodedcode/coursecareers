166e22fbec276ff9a9c0cc47b5ed7fb96f0166d5
Ryan <Woodedcode@gmail.com>
2025-11-13 21:03:11 -0600

    Add files via upload

---
+++ coursecareers/js/Array/array.js
@@ -0,0 +1,42 @@
+/*
+const arr = [1,2,3,4,5,"six",[7,8,9]]
+// console.log(arr[9])
+arr[arr.length + 2] = 100;
+
+console.log(arr.length)
+*/
+
+/*
+const arr = []
+
+const name1 = prompt("enter you name");
+const age = prompt("enter your age");
+const hairColor = prompt("enter your hair color");
+
+if (name1 !== "")arr.push(name1);
+if (age !== "") arr.push(age);
+if (hairColor !== "") arr.push(hairColor);
+
+alert(arr);
+const del = prompt("would you like to delete the last element?")
+
+if (del === 'yes') arr.pop();
+alert(arr)
+
+console.log(arr)
+*/
+/*
+const arr = []
+const arr2 = [1,2,3]
+const arr3 = [3,4,5]
+
+arr.push(arr2)
+arr.push(arr3)
+
+console.log(arr[1][0])
+*/
+
+let arr = [10,20,30];
+
+arr[0] = 40
+console.log(arr)
---
+++ coursecareers/js/Switch/switch.js
@@ -0,0 +1,37 @@
+const age = Number(prompt("Enter your age: "))
+
+// if (color === "red") {
+//     alert('That is my favorite color')
+// } else if (color === "blue"){
+//     alert("that is my second favorite color")
+// } else if (color === "pink") {
+//     alert("That is my wifes favorite color")
+// } else if (color === "orange") {
+//     alert("that is my daughters favorite color")
+// } else if (color === "green") {
+//     alert("weed is green")
+// } else {
+//     alert("please tell me your color")
+// }
+
+switch (age) {
+    case 10: 
+        alert("you are 10");
+        break;
+    
+    case 20:
+        alert("you are 20");
+        break;
+
+    case 30:
+        alert("you are 30")
+        break;
+
+    case 40:
+        alert("you are 40")
+        break;
+
+    default:
+        alert("you are old")
+}
+
