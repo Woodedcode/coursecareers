a3f0cddcca1474e8c7430231b8ed17083a1016da
Ryan <Woodedcode@gmail.com>
2025-11-13 22:33:47 -0600

    Add files via upload

---
+++ coursecareers/js/String-Methods/sm.js
@@ -0,0 +1,27 @@
+// const str = "hello world my      name is ryan"
+// const str2 = str.slice(6,8)
+// const str2 = str.replace("hello","!")
+// const str2 = str.replaceAll("l","!")
+// const str2 = str.toUpperCase()
+// const str2 = str.toUpperCase()
+// const str2 = str.trim();
+// const arr = str.split(" ")
+
+
+
+
+
+// console.log(str)
+// console.log(arr)
+
+
+
+
+const str = "hello world";
+const str2 = str.slice(0, 5);
+console.log(str2);
+// console.log(str2);
+
+
+
+
