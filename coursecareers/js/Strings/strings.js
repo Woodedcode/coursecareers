9addbf60a752d5717c8cc00dea769a18ad3841f6
Ryan <Woodedcode@gmail.com>
2025-11-13 22:33:31 -0600

    Add files via upload

---
+++ coursecareers/js/Strings/strings.js
@@ -0,0 +1,12 @@
+console.log("''\'")
+console.log("\\")
+
+console.log("hello\n\tworld")
+
+const name1 = "Ryan"
+
+const str = `hello ${name1} world`
+// const str2 = "hello" + name1 + " world"
+
+str[0] = "a"
+console.log(str)
