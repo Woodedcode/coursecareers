c5335482abfa2ab7850b758ebcbf8483d3c76e24
Ryan <Woodedcode@gmail.com>
2025-11-13 22:00:35 -0600

    Add files via upload

---
+++ coursecareers/js/Array-Methods/am.js
@@ -0,0 +1,28 @@
+// const arr = [45,23,45,-8,7]
+// const stringArr = arr.toString();
+
+// console.log(stringArr,arr)
+
+// arr.push()
+// arr.pop()
+
+// const removed = arr.shift()
+// console.log(arr,removed)
+// arr.unshift(-1)
+// arr.unshift(-2)
+// console.log(arr)
+// arr.sort((a,b) => a - b)
+// const arr2 = ["hello", "world"]
+// const arr3 = arr.concat(arr2)
+// const removed = arr.splice(2,3)
+// const str = arr.join(",");
+// const sl = arr.slice(1,5);
+
+
+
+// console.log(arr,sl)
+
+const arr = [1, 2];
+const arr2 = [3, 4];
+const arr3 = arr.concat(arr2);
+console.log(arr3)
