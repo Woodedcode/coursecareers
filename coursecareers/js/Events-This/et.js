6c092ab86fce41c4e74d08275df9431cce832cea
Ryan <Woodedcode@gmail.com>
2025-11-30 22:22:08 -0600

    Add files via upload

---
+++ coursecareers/js/Events-This/et.js
@@ -0,0 +1,42 @@
+// function enterusername(e) {
+    // const username = document.getElementById("username").value;
+    // document.getElementById("text").innerHTML = username;
+//     e.value = ''
+//     alert(e)
+// }
+
+
+// const person = {
+//     name: "Ryan",
+//     lastName: "Woods",
+//     getName: function() {
+//         console.log(this.name + this.lastName)
+//     }
+// }
+
+// person.getName()
+
+// function test() {
+//     console.log("Test")
+// }
+
+// const v = test;
+// v()
+
+// function test() {
+//     console.log(this)
+// }
+
+// this();
+
+const car = {
+    name: "Tesla Model S",
+    getName: function() {
+        console.log(this.name);
+    }
+}
+
+console.log(car)
+
+
+
