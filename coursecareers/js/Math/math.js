1dc4a5828db929347ae9d4256c8945ce06754ce6
Ryan <Woodedcode@gmail.com>
2025-12-06 08:07:01 -0600

    Add files via upload

---
+++ coursecareers/js/Math/math.js
@@ -0,0 +1,67 @@
+// rounding
+// const num = 2.55
+// const roundedNum = Math.round(num * 10) / 10
+// console.log(roundedNum)
+// LOWEST
+// const roundedNum = Math.floor(num)
+// console.log(roundedNum)
+// HIGHEST
+// const roundedNum = Math.ceil(num)
+// console.log(roundedNum)
+
+// const x = 10;
+// const y = 20;
+
+// MAX
+// const max = Math.max(x,y);
+// console.log(max)
+// const max = Math.max(x,y,30,-10, 8, 89);
+// console.log(max)
+// MIN
+// const max = Math.min(x,y,30,-10, 8, 89);
+// console.log(max)
+
+// USING ARRAYS
+// MIN
+// const arr = [1,2,-3,4,5]
+// const min = Math.min(...arr);
+// console.log(min)
+// MAX
+// const arr = [1,2,-3,4,5]
+// const max = Math.max(...arr);
+// console.log(max)
+
+// TO GET ABSOLUTE VALUE
+// const abs = Math.abs(-9)
+// console.log(abs)
+// RETURNS NATURAL LOG
+// const log = Math.log2(9)
+// console.log(log)
+
+
+// INFINITY
+const x = -Infinity
+console.log(x)
+
+
+const num = 2.6
+const roundedNum = Math.round(num)
+console.log(roundedNum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
