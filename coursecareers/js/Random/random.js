836e325b4e877be8d828058bb964032105ab9703
Ryan <Woodedcode@gmail.com>
2025-12-12 21:30:20 -0600

    Update random.js

--- coursecareers/js/Random/random.js
+++ coursecareers/js/Random/random.js
@@ -1,68 +1,50 @@
-function randomNum(min,max) {
+// const num = Math.random();
+// console.log(num)
+// GET A WHOLE NUMBER
+// const num = Math.round(Math.random() * 10);
+// console.log(num)
+// RANDOM NUMBER BETWEEN 10-20
+// const num = 10 + Math.round(Math.random() * 10);
+// console.log(num)
+//FUNCTION FOR RANDOM NUMBER
+// function randomNum(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min
+// }
+
+// const num = randomNum(20,80)
+// console.log(num)
+
+function randomNum(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
 let num = randomNum(1,100);
-let numGuesses = 0;
+let numGuesses = 0
 
 function reset() {
     num = randomNum(1,100);
-    numGuesses = 0;
-    document.getElementById("num-guesses").innerHTML = 0
+    numGuesses = 0
+    document.getElementById("num-guesses").innerHTML = 0;
 }
 
 function makeGuess() {
-    const guessInput = document.getElementById("guess");
+    const guessInput = document.getElementById("guess")
     const guess = Number(guessInput.value);
-    guessInput.value = ""
+    guessInput.value = "";
     const feedback = document.getElementById("feedback");
 
     numGuesses++;
-    document.getElementById("num-guesses").innerHTML = numGuesses
+    document.getElementById("num-guesses").innerHTML = numGuesses;
 
     if (guess === num) {
-        feedback.innerHTML = "You guessed it!";
-        reset()
+        feedback.innerHTML = "You guessed it";
+        reset();
+    } else if (guess > num) {
+        feedback.innerHTML = "It is lower.";
     } else {
-        feedback.innerHTML = "You did not guess it!";
+        feedback.innerHTML = "It is higher.";
     }
 }
-
-
-
-
-
-
-
-
-
-// RANDOM
-
-// const num = Math.round(Math.random() * 10)
-// console.log(num)
-// BETWEEN 10 - 20
-// const num = 10 + Math.round(Math.random() * 10)
-// console.log(num)
-
-
-
-// function randomNum (min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-// }
-// const num = randomNum(20,80)
-// console.log(num)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
