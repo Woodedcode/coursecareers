00c9d127bc48f64bd1d889825b74776773eed5d7
Ryan <Woodedcode@gmail.com>
2025-12-22 23:21:16 -0600

    Refactor password generation functions
    Refactor password generation logic and improve character selection.

--- coursecareers/js/Random-Password-Gen/rpg.js
+++ coursecareers/js/Random-Password-Gen/rpg.js
@@ -1,28 +1,21 @@
-// Length
-// Type of characters
-    //-Capitals
-    //-Lowercases
-    //-Special Character
-    //-Number
-//Display PW
-
 function getPasswordLength() {
     const length = document.getElementById("length").value;
     return Number(length);
 }
 
 function getPasswordProperties() {
-    const ids = ["lowercase","uppercase","numbers","special"];
+    const ids = ["lowercase","uppercase", "numbers", "special"]
     const properties = {};
 
     for (const id of ids) {
         const element = document.getElementById(id);
-        properties(id) = element.checked;
+        properties[id] = element.checked;
     }
-    return properties;
+
+    return properties
 }
 
-function getChars(lowercase = true) {
+function getChars(lowercase=true) {
     const start = lowercase ? 97 : 65;
     let chars = [];
 
@@ -33,26 +26,46 @@ function getChars(lowercase = true) {
 }
 
 function getNumbers() {
-    const nums = []
+    const nums = [];
 
-    for (let i = 0; i< 10; i++) {
+    for (let i = 0; i < 10; i++) {
         nums.push(i)
     }
+
     return nums
 }
+
+
+    const lowercaseChars = getChars(true);
+    const uppercaseChars = getChars(false);
+    const numbers = getNumbers();
+    const special = ["!","@","#","$","%","^","&","*","(",")","_","+"]
 
 function generatePassword() {
     const length = getPasswordLength();
     const properties = getPasswordProperties();
-    const lowercaseChars = getChars(true);
-    const uppercaseChars = getChars(false);
-    const numbers = getNumbers();
-    const special = ["!","@","#","^","*","&","(",")"]
 
-    console.log(lowercaseChars,uppercaseChars)
+    const characters = [];
+    if (properties.lowercase) characters.push(...lowercaseChars);
+    if (properties.uppercase) characters.push(...uppercaseChars);
+    if (properties.numbers) characters.push(...numbers);
+    if (properties.special) characters.push(...special);
+    if (characters.length === 0) {
+        return alert("You must select at least one set of characters.")
+    }
+
+    let pwd = [];
+    
+    for (let i = 0; i < length; i++) {
+        const randomIdx = Math.floor(Math.random() * characters.length);
+        const char = characters[randomIdx];
+        pwd.push(char)
+    }
+    const pwdString = pwd.join("")
+    document.getElementById("password").innerHTML = "<p>" + pwdString + "</p>";
 }
 
-
+//console.log(length,properties,lowercaseChars,uppercaseChars,numbers,special)
 
 
 
