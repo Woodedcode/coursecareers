7c36a218e20d9cea9a4faf6ceb2b7507bb272f6e
Ryan <Woodedcode@gmail.com>
2026-01-15 21:30:47 -0600

    Update Home component to display multiple posts
    Refactored Home component to include an array of posts with titles, authors, and content.

--- coursecareers/app/page.js
+++ coursecareers/app/page.js
@@ -1,5 +1,5 @@
 1/15/26
-right click file in VS code to get right path for folder
+//right click file in VS code to get right path for folder
 
 import Post from "@/components/Post";
 
@@ -13,5 +13,34 @@ export default function Home() {
       title="Ryan is new to programming"
       author="Ryan Woods"
       content="Ryan is very green to this but it getting better"/>
+    </div>
+}
+
+
+1/15/26
+//updated through lessons
+
+import Post from "@/components/Post";
+
+export default function Home() {
+  const post = [{
+    title:"My Post",
+    author: "Ryan Woods",
+    content: "I am an 'ok' react developer",
+    following: false},
+    {
+    title:"My Post 2",
+    author: "Ryan Woods",
+    content: "just testing things out",
+    following: true},
+    {
+    title:"My Post 3",
+    author: "Ryan Woods",
+    content: "this is another post",
+    following: false}]
+  return <div>
+    {post.map((content,idx) => {
+      return <Post {...content} key={idx}/>
+    })}
     </div>
 }
