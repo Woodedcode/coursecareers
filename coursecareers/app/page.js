c67c32b5a06636b5c7e89d0eb1c19286059ea577
Ryan <Woodedcode@gmail.com>
2026-01-15 20:42:08 -0600

    Create Home component with Post entries

---
+++ coursecareers/app/page.js
@@ -0,0 +1,17 @@
+1/15/26
+right click file in VS code to get right path for folder
+
+import Post from "@/components/Post";
+
+export default function Home() {
+  return <div>
+      <Post 
+      title="Ryan is new to programming"
+      author="Ryan Woods"
+      content="Ryan is very green to this but it getting better"/>
+      <Post 
+      title="Ryan is new to programming"
+      author="Ryan Woods"
+      content="Ryan is very green to this but it getting better"/>
+    </div>
+}
