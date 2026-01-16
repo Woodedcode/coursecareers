855dbbffa9d8c913daf2e3da623ba839574dde74
Ryan <Woodedcode@gmail.com>
2026-01-15 20:42:42 -0600

    Add Post component to display title, author, and content

---
+++ coursecareers/app/Post.js
@@ -0,0 +1,11 @@
+1/15/26
+
+export default function Post({ title,author,content}) {
+    return (
+    <div>
+        <h1>{title}</h1>
+        <h3>By: {author}</h3>
+        <p>{content}</p>
+    </div>
+    );
+}
