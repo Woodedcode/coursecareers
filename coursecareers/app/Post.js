f3d0e7dfb662d1925361f58a5b9ca234c23b4fe6
Ryan <Woodedcode@gmail.com>
2026-01-15 21:31:35 -0600

    Add follow/unfollow button to Post component

--- coursecareers/app/Post.js
+++ coursecareers/app/Post.js
@@ -9,3 +9,20 @@ export default function Post({ title,author,content}) {
     </div>
     );
 }
+
+1/15/26
+//updated through lessons
+import "./post.css";
+
+
+export default function Post({ title,author,content,following}) {
+    const button = !following ? (<button>Follow</button>) : (<button>Unfollow</button>)
+    return (
+    <div className="card">
+        <h1>{title}</h1>
+        <h3>By: {author}</h3>
+        {button}
+        <p>{content}</p>
+    </div>
+    );
+}
