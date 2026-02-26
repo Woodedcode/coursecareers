23f4ade704d26610f590cff30ad8f4be2de858de
Ryan <Woodedcode@gmail.com>
2026-02-25 23:12:49 -0600

    Refactor Post component structure and import CSS

--- coursecareers/app/Post.js
+++ coursecareers/app/Post.js
@@ -76,3 +76,22 @@ const button = !following ? (
     </div>
     );
 }
+
+
+2/25/26
+
+import "./post.css"
+
+export default function Post({ title, author, content}) {
+
+
+    return (
+    <div className="card">
+        <h1>{title}</h1>
+        <h3>By: {author}</h3>
+        <p>{content}</p>
+    </div>
+    );
+}
+
+
