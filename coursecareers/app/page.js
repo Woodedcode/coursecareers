47cb39ad3c9e615d1a2f904fc3d6dac45cb02a62
Ryan <Woodedcode@gmail.com>
2026-02-25 23:13:21 -0600

    Implement post creation functionality in Home component

--- coursecareers/app/page.js
+++ coursecareers/app/page.js
@@ -136,3 +136,73 @@ export default function Home() {
     </div>
   );
 }
+
+
+2/25/26
+
+// function Text({ content,strong }) {
+//   if (strong) {
+//     return <strong>{content}</strong>
+//   }
+
+//   return <p>{content}</p>
+// }
+"use client";
+import Post from "@/components/Post"
+import {useState, useEffect} from "react";
+
+export default function Home() {
+  const [content, setContent] = useState("");
+  const [author, setAuthor] = useState("");
+  const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    if (content.length >= 10) alert("Your content is getting kind of long")
+  }, [content])
+
+  function addPost() {
+    const newPost = {
+      author: author,
+      content: content,
+      title: title,
+      following: false
+    }
+
+    setPosts([...posts, newPost])
+    setContent("");
+    setAuthor("");
+    setTitle("");
+  }
+
+  return (
+    <div>
+      <div>
+        <input 
+        type="text" 
+        value={title} 
+        onChange={(e) => setTitle(e.target.value)} 
+        placeholder="Title"/>
+
+        <input 
+        type="text" 
+        value={content} 
+        onChange={(e) => setContent(e.target.value)} 
+        placeholder="Content"/>
+
+        <input 
+        type="text" 
+        value={author} 
+        onChange={(e) => setAuthor(e.target.value)} 
+        placeholder="Author"/>
+
+        <button onClick={addPost}>Add Post</button>
+      </div>
+      <div className="posts">
+        {posts.map((posts, idx) => (
+          <Post {...posts} key={idx + Math.random()}/>
+        ))}
+      </div>
+    </div>
+  )
+}
