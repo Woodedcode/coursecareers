af447754e8276530541a218abaed4db1e5e43a80
Ryan <Woodedcode@gmail.com>
2026-02-26 21:42:05 -0600

    Add files via upload

--- coursecareers/app/about/page.js
+++ coursecareers/app/about/page.js
@@ -1,7 +1,24 @@
-export default function About() {
-    return(
-        <div>
-            <h1>About</h1>
-        </div>
-    );
+"use client"
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Home() {
+const [userId, setUserId] = useState("");
+const router = useRouter();
+
+function navigate() {
+  router.push("/user/" + userId);
+}
+
+  return (
+    <div>
+      <Link href="/about/contact">Contact</Link>
+      <Link href="/about">About</Link>
+      <div>
+          <input type="text" placeholder="user id" value={userId} onChange={ (e) => setUserId(e.target.value)} id/>
+          <button onClick={navigate}>Go to Profile</button>
+      </div>
+    </div>
+  )
 }
