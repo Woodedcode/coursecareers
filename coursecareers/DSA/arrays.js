f3737d67e5665cb4acff7e457a80047334d9e97b
Ryan <Woodedcode@gmail.com>
2026-03-30 22:58:15 -0500

    Add files via upload

---
+++ coursecareers/DSA/arrays.js
@@ -0,0 +1,6 @@
+// Fixed (static) - cannot change its size
+// Dynamic - increase or decrease its size
+
+
+
+a = [3,4,5,6]
---
+++ coursecareers/DSA/dsa-explained.js
@@ -0,0 +1,69 @@
+/*
+Data Structures
+a named location that can be used to store and organize data EX - family tree
+Linear
+-Array
+-linked list
+-Stack
+-Queue
+
+
+Non-Linear
+-Tree
+-Graphs
+-Tries
+NOT 
+
+same this as Data Types
+Boolean
+Strings
+Int
+
+What is an Algorithm
+a collection of steps to solve a problem EX - bake a pizza
+Linear search - one by one, examine the elements of an array to find a value
+
+
+
+STACK - LIFO data structure. Last-In-First-Out
+stores objects into a sort of "vertical tower"
+push() to add to the top
+pop() to remove from the top
+
+stack.push(minecraft)
+stack.push(skyrim)
+stack.push(doom)
+stack.push(borderlands)
+stack.push(call of duty)
+
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+stack.pop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
