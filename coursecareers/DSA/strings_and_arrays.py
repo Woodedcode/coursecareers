3e7ce53b660c3a00a60090128987eae986039a17
Ryan <Woodedcode@gmail.com>
2026-04-14 22:49:13 -0500

    Refactor string manipulation examples in Python

--- coursecareers/DSA/strings_and_arrays.py
+++ coursecareers/DSA/strings_and_arrays.py
@@ -1,116 +1,193 @@
-# # 'a' === "a"
+# # # 'a' === "a"
 
+# # print(type('a'))
+# # print(type("a"))
+
+# # # strings in python are immutable
+
+# # # name = 'Ryan'
+# # # last = 'Woods'
+
+# # # print(id(name))
+
+# # # name +=  " " + last
+
+# # # print(id(name))
+# # # print(name)
+
+
+
+# # # words = ["hello", "my", "name", "is", "Ryan"]
+# # # final = words[0]
+
+# # # for word in words[1:]:
+# # #     final += " "
+# # #     final += word
+
+# # # print(final)
+
+# # # string input
+# # # convert to an array
+# # # convert back to a string
+
+# # # words1 = ["hello", "my", "name", "is", "Ryan"]
+
+# # # # final1 = " ".join(words1)
+# # # # print(final1)
+
+
+# # # message = "Hello my name is Ryan"
+
+# # # words2 = message.split()
+# # # # do anything with the array
+
+# # # final2 = " ".join(words2)
+# # # print(final2)
+
+
+# # import time
+# # words = ["pneumonoultramicroscopicsilicovolcanoconiosis"] * 100000
+# # print(words)
+
+# # start = time.time()
+
+# # # Does the work
+# # for word in words:
+# #     final += word
+
+# # # final = " ".join(words)
+
+# # end = time.time()
+
+# # print(f"{end - start:.6f}")
+
+
+################################################## # 4/13/26
 # print(type('a'))
-# print(type("a"))
 
-# # strings in python are immutable
+# # strings are immutable
 
-# # name = 'Ryan'
-# # last = 'Woods'
 
-# # print(id(name))
+# name = "Ryan"
+# last = 'Woods'
 
-# # name +=  " " + last
+# print(id(name))
+# name += " " +  last
+# print(id(name))
+# print(name)
 
-# # print(id(name))
-# # print(name)
 
 
 
 # # words = ["hello", "my", "name", "is", "Ryan"]
+
+# # # final = words[0]
+
+# # # for word in words[1:]:
+# # #     final += " "
+# # #     final += word
+
+# # # print(final)
+
+# # final = " ".join(words)
+# # print(final)
+
+
+# # #string input
+# # #convert to an array
+# # #convert back to a string
+# # message = "Howdy my name is Ryan"
+
+# # words = message.split()
+# # #do anything with the array
+# # print(words)
+
+# # final = " ".join(words)
+# # [print(final)]
+
+# import time 
+
+# words = ['pneumonoultramicroscopicsilicovolcanoconiosis'] * 100000
+
+# print(words)
+
+# start = time.time()
+
+# # final = ""
+
+# # for word in words:
+# #     final += word
+
+
+# final = " ".join(words)
+
+# end = time.time()
+
+# print(f"{end-start:.6f}")
+
+
+
+################################################## # 4/14/26
+# print(type('a'))
+# #strings in python are immutable
+# name = "Ryan"
+# last = "Woods"
+# print(id(name))
+# name += " " + last
+# print(id(name))
+# print(name)
+
+
+# words = ['hello', 'my', 'name', 'is', 'Ryan']
+
+# ###################### LONG WAY
 # # final = words[0]
 
 # # for word in words[1:]:
 # #     final += " "
 # #     final += word
 
-# # print(final)
 
-# # string input
-# # convert to an array
-# # convert back to a string
+# ###################### SHORT WAY
+# final = " ".join(words)
 
-# # words1 = ["hello", "my", "name", "is", "Ryan"]
-
-# # # final1 = " ".join(words1)
-# # # print(final1)
+# print(final)
 
 
-# # message = "Hello my name is Ryan"
 
-# # words2 = message.split()
-# # # do anything with the array
+# #string input
+# #convert to an array
+# #convert back to a string
+# message = "Hewllo my name is Ryan"
 
-# # final2 = " ".join(words2)
-# # print(final2)
+# words = message.split()
+# #do anything with the array
+
+# final = " ".join(words)
+# print(final)
 
 
+# ###################### TIMING
 # import time
-# words = ["pneumonoultramicroscopicsilicovolcanoconiosis"] * 100000
-# print(words)
+
+
+# words = "pneumonoultramicroscopicsilicovolcanoconiosis" * 100000
+
 
 # start = time.time()
 
-# # Does the work
-# for word in words:
-#     final += word
+# final = ""
 
-# # final = " ".join(words)
+# # for word in words:
+# #     final += word
+
+# final = "".join(words)
 
 # end = time.time()
 
 # print(f"{end - start:.6f}")
 
 
-
-
-
-
-
-
-
-# 4/13/26
-print(type('a'))
-
-# strings are immutable
-
-
-name = "Ryan"
-last = 'Woods'
-
-print(id(name))
-name += " " +  last
-print(id(name))
-print(name)
-
-
-
-
-# words = ["hello", "my", "name", "is", "Ryan"]
-
-# # final = words[0]
-
-# # for word in words[1:]:
-# #     final += " "
-# #     final += word
-
-# # print(final)
-
-# final = " ".join(words)
-# print(final)
-
-
-# #string input
-# #convert to an array
-# #convert back to a string
-# message = "Howdy my name is Ryan"
-
-# words = message.split()
-# #do anything with the array
-# print(words)
-
-# final = " ".join(words)
-# [print(final)]
 
 import time 
 
@@ -120,14 +197,15 @@ print(words)
 
 start = time.time()
 
-# final = ""
+final = ""
 
-# for word in words:
-#     final += word
+for word in words:
+    final += word
 
 
-final = " ".join(words)
+# final = " ".join(words)
 
 end = time.time()
 
 print(f"{end-start:.6f}")
+
