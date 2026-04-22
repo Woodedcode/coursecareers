bc5f48c4bd82f9840fd074ffd08f44b7f2892c18
Ryan <Woodedcode@gmail.com>
2026-04-21 23:12:33 -0500

    Enhance dynamic_arrays.py with detailed comments
    Added comprehensive comments and explanations about static and dynamic arrays, including their properties and operations.

--- coursecareers/DSA/dynamic_arrays.py
+++ coursecareers/DSA/dynamic_arrays.py
@@ -97,5 +97,189 @@
 
 
 
+######################################### 4/20/26
+#STATIC AND DYNAMIC ARRAYS
+
+# Data the contains multiple type of data typpe -> num>str>boolean
+# array is a collection and has alot of built in capabilities
+#could be considered as data type or data structure
+
+
+# contiguous memory is vitally important for an array
+#  Dynmic array is an array that can grow and shrink
+
+# data = ["data", 5, True, [5,3,1]]
+# print(data)
+
+#layer of indirection
+#behind the scenes - Array will store pointer to the data
+#the pointers are contiguous
+# supports random access
+
+
+# random access - in constant time -> So you might see 0(1)
+# contiguous
+# consistently sized
+
+
+# print(data[2])
+# startign memory location + (size of each element * index being used)
+
+# 100
+# 4
+# [2]
+# 100 + 8 = 108
+# if its a pointer, just go to wherever the pointer points
+
+
+# data = [1,2,3,4,5]
+
+# # data.extend([6,7,8,9,10]) #in-place - adjust algorithm -> operation or extending an array -> adding to the original memory -> modify iriginal variable
+
+# data + [6,7,8,9,10]
+# original array stays unchanged
+
+
+# print(data)
+
+
+# data1 = [54,23,656,12,87]
+# data2 = sorted(data1)
+# print(data1,data2)
+
+
+# data = [54,23,656,12,87]
+# data.sort()
+
+# [print(data)]
+
+# COPYING
+# data1 = [1,2,3]
+# data2 = data1[:] # : -> slice /// .copy()
+# data2[0] = 100
+# print(data1)
+
+
+# data1 = [0,1,2,3,4,5,6,7,8]
+# # data2 = data1[3:5]
+# # [inclusive:exclusive]
+# # data2 = data1[::-1]
+# data1.reverse()
+# # print(data2)
+# print(data1)
+
+# LOOPING WITH ARRAYS
+# data = [0,1,2,3,4,5,6,7,8]
+# data = ["hello", "my", "name", "is", "Ryan"]
+
+# for i in range(len(data)):
+#     print(i)
+# #prints value
+
+# for d in data:
+#     print(d)
+# # prints data
+
+# for i in range(len(data)):
+#     print(i, data[i])
+# # prints value and data
+
+# for i in range(len(data) -1, -1, -1):
+#     print(i,data[i])
+
+# LIST COMPREHENSION
+# data = ["hello", "my", "name", "is", "Ryan"]
+# final = [str.upper(word) for word in reversed(data)]
+
+# print(final,data)
+
+
+
+# import time 
+
+# words = ['pneumonoultramicroscopicsilicovolcanoconiosis'] * 100000
+
+# print(words)
+
+# start = time.time()
+
+# # final = ""
+
+# # for word in words:
+# #     final += word
+
+
+# final = " ".join(words)
+
+# end = time.time()
+
+# print(f"{end-start:.6f}")
+
+
+
+# data = ['data', 5 ,True, [5,3,1]]
+# print(id(data))
+#layer of inderection
+#behind the scenes - Array will store pointers to the data
+#pointers are contigousfd
+
+
+
+
+# data = ['data', 5 ,True, [5,3,1]]
+# #random access
+# #contigous
+# #consistently sized
+# print(data[2])
+
+#starting memory location + (size of each element * index baing used)
+
+# 100
+# 4
+# 2
+
+
+# 100 + 8 = 108
+
+
+# data = [ 1,2,3,4,5]
+# data.extend([6,7,8,9,10]) in-place - alters original memory
+# print(data)
+
+
+# data = [ 1,2,3,4,5]
+# data += [6,7,8,9,10]
+# print(data)
+
+
+
+# data1 = [54,23,656,12,8,10,45]
+# data2 = sorted(data1)
+
+# print(data1, data2)
+#if wanted to sort
+
+
+
+# data = [54,23,656,12,87]
+# data.sort()
+
+# print(data)
+
+# COPYING
+data = [1,2,3] 
+
+
+data1 = [1,2,3]
+# data2 = data1 [:]
+
+
+data2[2] = 100
+print(data1)
+
+
+
+
+
 
 
