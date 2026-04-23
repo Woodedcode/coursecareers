8be6ec68dfb04b8ae1ff54d0704b4a86bbefcc95
Ryan <Woodedcode@gmail.com>
2026-04-22 22:10:05 -0500

    Add files via upload

---
+++ coursecareers/DSA/abstract-data.py
@@ -0,0 +1,71 @@
+# stack, queue, deque, tree, graph
+
+# interface - set of rules that must be met or implemented
+
+
+# STACK
+# two different operations - push and pop
+# last in, first out
+stack = []
+
+stack.append(5)
+print(stack)
+stack.append(10)
+print(stack)
+stack.append(15)
+print(stack)
+stack.pop()
+print(stack)
+print(stack.pop()e)
+
+# QUEUE
+# first in first out
+
+
+
+# DEQUE
+
+
+
+# PRIORITY QUEUE
+
+
+
+
+# SET
+
+
+
+
+# TREE
+
+
+
+# GRAPH
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
