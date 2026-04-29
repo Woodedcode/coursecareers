2db2399696bd5d3d1e1061153b28bc0869f017bd
Ryan <Woodedcode@gmail.com>
2026-04-28 21:51:02 -0500

    Restore deque performance measurement code
    Reintroduced deque and time imports, and restored code for measuring performance of deque operations.

--- coursecareers/DSA/abstract-data.py
+++ coursecareers/DSA/abstract-data.py
@@ -105,22 +105,22 @@
 
 #SET - HASHING
 #DEQUE
-from collections import deque
-import time
+# from collections import deque
+# import time
 
-data = deque([])
+# data = deque([])
 
 
-start = time.time()
-for i in range(200000):
-    data.insert(0,i)
+# start = time.time()
+# for i in range(200000):
+#     data.insert(0,i)
 
-while data:
-    print(data.pop())
+# while data:
+#     print(data.pop())
 
-end = time.time()
+# end = time.time()
 
-print(f"{end - start:.6f} seconds")
+# print(f"{end - start:.6f} seconds")
 
 
 
@@ -132,6 +132,96 @@ print(f"{end - start:.6f} seconds")
 #ASSOCIATIVE ARRAY
 #TREE
 #GRAPH
+
+
+
+
+
+##############################    4/28/26
+# interface - defined as an abstract type
+
+#stack - had an agreement to two different operations - push() and pop()
+# push and pop is the interface for the stack.
+
+# Most common ADT's - Stack, queue, deque, priority queue, set, associative array, tree, graph
+
+# Stacks - must add an element to the collection and remove the most recent element from the collection - LIFO
+#push()
+#pop()
+
+
+# Queue
+# Enqueue - which adds one element to the rear of the queue
+
+# Dequeue - which removes one element from the front of the queue
+
+
+# The operation of a queue makes it a first-in-first-out FIFO
+
+# Double ended queues
+
+# allows you to do those oeprations on BOTH sides of the data
+
+# allows a peek operation
+
+#################################### STACK
+# stack = []
+
+# stack.append(5)
+# print(stack)
+# stack.append(10)
+# print(stack)
+# stack.append(15)
+# print(stack)
+
+# print("peek", stack[-1])
+
+# print(stack.pop())
+# print(stack)
+# print(stack.pop())
+# print(stack)
+# print(stack.pop())
+# print(stack)
+
+
+
+
+
+#################################### QUEUE
+# q = []
+# q.insert(0,5)
+# print(q)
+# q.insert(0,10)
+# print(q)
+# q.insert(0,15)
+# print(q)
+
+# print(q.pop())
+# print(q)
+# print(q.pop())
+# print(q)
+# print(q.pop())
+# print(q)
+
+
+#################################### DOUBLE ENDED QUEUE - custom type - Elements can be added OR removed from either the front or back - head tail linked list
+from collections import deque
+import time
+
+data = deque([])
+
+start = time.time()
+
+for i in range(200000):
+    data.insert(0,i)
+
+while data:
+    data.pop()
+
+end = time.time()
+
+
+print(f"{end - start:.6f} seconds")
 
 
 
