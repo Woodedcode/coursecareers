26ebb0e21b93da9292adcfefd7be8836c3d7b95f
Ryan <Woodedcode@gmail.com>
2026-05-02 12:50:36 -0500

    Add files via upload

---
+++ coursecareers/DSA/oop.py
@@ -0,0 +1,13 @@
+# OBJECT ORIENTED PROGRAMING
+
+#programming paradigm
+
+#built off classes
+
+#find a class and think of a class as a blueprint
+
+#then you create instances of the class which are called Objects
+
+#linked list which will have a node which will have the value 5 and an arrow pointing to the next node which may have the value of 10
+
+
