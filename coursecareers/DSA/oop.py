afe1d0d0c00e53a4010c890f45b20a2fa703677e
Ryan <Woodedcode@gmail.com>
2026-05-03 22:24:27 -0500

    Enhance oop.py with OOP and linked list explanations
    Added comments explaining OOP concepts and linked lists.

--- coursecareers/DSA/oop.py
+++ coursecareers/DSA/oop.py
@@ -11,3 +11,12 @@
 #linked list which will have a node which will have the value 5 and an arrow pointing to the next node which may have the value of 10
 
 
+
+################################### 5/3/26
+
+#OOP - programming pparadigm - an approach to how to build your code and it is built off classes - think of a class as a blueprint that desribes the structure of the data - create instances of this class which are called objects
+
+
+#linked lists - youll create a node with a value of 5 and a next pointer to another node which may have the value 10 - easier with classes
+
+# working with classes, youll have a constructor or an initializer (_ _ init_ _()) - it is a method which is also a function attached to an object - init is invoked on instantiation 
